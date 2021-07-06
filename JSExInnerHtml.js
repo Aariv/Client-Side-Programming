48860ba3436235d255cd9f016586eafe34e5ff5b
Aariv <arivbits@gmail.com>
2021-07-06 12:51:04 +0530

    Worked on changing DOM of html

---
+++ JSExInnerHtml.js
@@ -0,0 +1,28 @@
+window.onload = init;
+
+function init() {
+	document.getElementById("btn1").onclick = changeHeading1;
+	document.getElementById("btn2").onclick = changeHeading2;
+	document.getElementById("btn3").onclick = changeParagraph;
+}
+
+function changeHeading1() {
+	var elm = document.getElementById("heading1");
+	elm.innerHTML = "Hello";
+}
+
+function changeHeading2() {
+	var elms = document.getElementsByTagName("h2");
+
+	for(var i = 0; i < elms.length; ++i) {
+		elms[i].innerHTML = "Hello Again!";
+	}
+}
+
+function changeParagraph() {
+	var elms = document.getElementsByClassName("para");
+
+	for(var i = 0; i < elms.length; ++i) {
+		elms[i].innerHTML = "Hello Again & again";
+	}
+}
